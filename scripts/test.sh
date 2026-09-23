6297d161605d8a48fe09659cb66ff37cbe2a1f48
AdayGuedes <munozguedesaday@gmail.com>
2026-09-23 10:55:58 -0400

    refactor: Remove single test run comment

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,6 @@
 
 #Command for this script: 
 # ./scripts/test.sh to run all the tests that are in the tests directory
-# ./scripts/test.sh name_of_test.py to run an specific test
-
 
 set -euo pipefail
 
